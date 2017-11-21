e054a6e8b9629bf695d9056fe03245851d83991b
Guyb <guybarnea22@gmail.com>
2017-11-21 20:23:25 +0200

    Add files via upload

--- tag-parser.scm
+++ tag-parser.scm
@@ -178,43 +178,43 @@ unquote-splicing quote set!))
 
 (define quote-const?
   (lambda (exp)
-  	(and (pair? exp) (eq? (car exp) 'quote))))
+  	(and  (pair? exp) (eq? (car exp) 'quote))))
 
 (define if?
   (lambda (exp)
-  	(and (list? exp) (= (length exp) 4) (eq? (car exp) 'if))))
+  	(and (list? exp) (eq? (car exp) 'if) (= (length exp) 4))))
 
 (define if-without-else?
   (lambda (exp)
-  	(and (list? exp) (= (length exp) 3) (eq? (car exp) 'if))))
+  	(and (list? exp) (eq? (car exp) 'if) (= (length exp) 3))))
 
 (define or?
   (lambda (exp)
-  	(and (pair? exp) (eq? (car exp) 'or))))
+  	(and (pair? exp)) (eq? (car exp) 'or)))
 
 (define lambda-simple?
   (lambda (exp)
-  	(and (list? exp) (> (length exp) 2) (check-simple-params? (cadr exp)) (eq? (car exp) 'lambda))))
+  	(and (list? exp) (eq? (car exp) 'lambda) (> (length exp) 2) (check-simple-params? (cadr exp)))))
 
 (define lambda-opt?
   (lambda (exp)
-	(and (list? exp) (> (length exp) 2) (improper-list? (cadr exp)) (eq? (car exp) 'lambda))))
+	(and (list? exp) (eq? (car exp) 'lambda) (> (length exp) 2) (improper-list? (cadr exp)))))
 
 (define lambda-var?
   (lambda (exp)
-	(and (list? exp) (> (length exp) 2) (var? (cadr exp)) (eq? (car exp) 'lambda))))
+	(and (list? exp) (eq? (car exp) 'lambda) (> (length exp) 2) (var? (cadr exp)))))
 
 (define regular-define?
   (lambda (exp)
-  	(and (list? exp) (> (length exp) 2) (var? (cadr exp)) (eq? (car exp) 'define))))
+  	(and (list? exp) (eq? (car exp) 'define) (> (length exp) 2) (var? (cadr exp)))))
 
 (define mit-style-define?
   (lambda (exp)
-	(and (list? exp) (> (length exp) 2) (list? (cadr exp)) (eq? (car exp) 'define))))
+	(and (list? exp) (eq? (car exp) 'define) (> (length exp) 2) (list? (cadr exp)))))
 
 (define set?
   (lambda (exp)
-	(and (list? exp) (> (length exp) 2) (var? (cadr exp)) (eq? (car exp) 'set!))))
+	(and (list? exp) (eq? (car exp) 'set!) (> (length exp) 2) (var? (cadr exp)))))
 
 (define applic?
   (lambda (exp)
@@ -222,16 +222,40 @@ unquote-splicing quote set!))
 
 (define let-without-bindings?
   (lambda (exp)
-	(and (list? exp) (> (length exp) 2) (null? (cadr exp)) (eq? (car exp) 'let))))
+	(and (list? exp) (eq? (car exp) 'let) (> (length exp) 2) (null? (cadr exp)))))
 
 
 (define let-with-bindings?
   (lambda (exp)
-	(and (list? exp) (> (length exp) 2) (eq? (car exp) 'let))))
+	(and (list? exp)  (eq? (car exp) 'let) (> (length exp) 2))))
 
 (define let-star-without-bindings?
   (lambda (exp)
-	(and (list? exp) (> (length exp) 2) (null? (cadr exp)) (eq? (car exp) 'let*))))
+	(and (list? exp) (eq? (car exp) 'let*) (> (length exp) 2) (null? (cadr exp)))))
+
+(define let-star-with-bindings?
+  (lambda (exp)
+	(and (list? exp) (eq? (car exp) 'let*) (> (length exp) 2))))
+
+(define letrec-without-bindings?
+  (lambda (exp)
+  	(and (list? exp) (eq? (car exp) 'letrec) (> (length exp) 2) (null? (cadr exp)))))
+
+(define letrec-with-bindings?
+  (lambda (exp)
+  	(and (list? exp) (eq? (car exp) 'letrec) (> (length exp) 2))))
+
+(define and-without-args?
+  (lambda (exp)
+  	(and (list? exp) (eq? (car exp) 'and) (= (length exp) 1))))
+
+(define and-with-one-arg?
+  (lambda (exp)
+  	(and (list? exp) (eq? (car exp) 'and) (= (length exp) 2))))
+
+(define and?
+  (lambda (exp)
+  	(and (list? exp) (eq? (car exp) 'and) (> (length exp) 2))))
 
 (define parse
 	(lambda (exp)
@@ -324,9 +348,47 @@ unquote-splicing quote set!))
 			   	(parse `(let () ,first-body ,@rest-bodies))))
 
 			   ;let-star-with-bindings
-			   ;((and (list? exp) (> (length exp) 2) (eq? (car exp) 'let*)))
-			   	
+			   ((let-star-with-bindings? exp)
+			   	(let ((handle-bindings (lambda (bindings body other-bodies)
+				  (if (null? bindings)
+				    `(,body ,@other-bodies)
+				    `((let* ,bindaings ,body ,@other-bodies)))))
+			   		  (key (caaadr exp))
+			   		  (val (cdaadr exp))
+			   		  (other-bindings (cdadr exp))
+			   		  (body (caddr exp))
+			   		  (other-bodies (cdddr exp)))
+				(parse `(let ((,key ,@val)) ,@(handle-bindings other-bindings body other-bodies)))))
 
+			   ;letrec-without-bindings
+			   ((letrec-without-bindings? exp)
+			   	(let ((first-body (caddr exp))
+			   		  (rest-bodies (cdddr exp)))
+			   	(parse `(let () (let () ,first-body  ,@rest-bodies)))))
+
+			   ;letrec-with-bindings
+			   ((letrec-with-bindings? exp)
+			   	(let* ((key (caaadr exp))
+			   		  (val (cdaadr exp))
+			   		  (other-bindings (cdadr exp))
+			   		  (body (caddr exp))
+			   		  (other-bodies (cdddr exp))
+			   		  (args-names (append (list key) (map car other-bindings)))
+			   		  (set-args (map (lambda (x) `(set! ,(car x) ,(cadr x))) (append (list (list key (car val))) other-bindings))))
+			   		  (parse `((lambda ,args-names ,@set-args ,`(let () ,body ,@other-bodies)) ,@(make-list (length args-names) #f)))))
+
+
+			   ;and-without-args
+			   ((and-without-args? exp) (parse #t))
+
+			   ;and-with-one-arg
+			   ((and-with-one-arg? exp) (parse (cadr exp)))
+
+			   ;and
+			   ((and? exp)
+			   (let ((first (cadr exp))
+			   		(rest (cddr exp)))
+			   `(if3 ,(parse first) ,(parse `(and ,@rest)) ,(parse #f))))
 
 
 
